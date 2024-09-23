df2a30798787e8548a34f16a62ae531799780261
apple-yagi <applesryu@gmail.com>
2024-09-23 21:53:56 +0900

    feat: Update dependencies and add CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+use clap::Parser;
 use classsniffer::classnames;
 use std::fs;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct CliArgs {
+    /// Target CSS file (default: stdin)
+    #[clap(value_parser)]
+    input_file: Vec<String>,
+}
+
 fn main() {
+    let cli_args = CliArgs::parse();
+
     // CSSファイルを読み込み
-    let css_file_path = "testdata/styles.css"; // ここにCSSファイルのパスを指定
+    let css_file_path = &cli_args.input_file[1];
     let css_content = fs::read_to_string(css_file_path).expect("Failed to read the CSS file");
 
     // クラス名を抽出
