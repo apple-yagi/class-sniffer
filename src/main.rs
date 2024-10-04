b9e5d3aa8fdb0b6538b7fb018803ffdfb2226f46
apple-yagi <applesryu@gmail.com>
2024-10-04 17:41:19 +0900

    feat: display diff classname

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,11 @@ struct CliArgs {
 }
 
 fn main() {
-    let cli_args = CliArgs::parse();
+    // let cli_args = CliArgs::parse();
 
     // CSSファイルを読み込み
-    let css_file_path = &cli_args.input_file[1];
-    let css_content = fs::read_to_string(css_file_path).expect("Failed to read the CSS file");
+    // let css_file_path = &cli_args.input_file[1];
+    let css_content = fs::read_to_string("testdata/styles.css").expect("Failed to read the CSS file");
 
     // クラス名を抽出
     let css_classnames = css::extract_classname(&css_content);
@@ -24,14 +24,21 @@ fn main() {
     let tsx_content = fs::read_to_string("testdata/test.tsx").expect("Failed to read the Tsx file");
     let tsx_classnames = tsx::extract_classname(&tsx_content);
 
-    // 結果を表示
-    println!("Classnames in the CSS file:");
-    for classname in css_classnames {
-        println!("{}", classname);
+    // TSXファイルに存在するクラス名のうち、CSSファイルに存在しないものを抽出
+    let diff_classnames = tsx_classnames
+        .iter()
+        .filter(|classname| !css_classnames.contains(classname))
+        .collect::<Vec<_>>();
+
+    // diff_classnamesが空でない場合、エラーを出力
+    if !diff_classnames.is_empty() {
+        eprintln!("The following classnames are not defined in the CSS file:");
+        for classname in diff_classnames {
+            eprintln!("  - {}", classname);
+        }
+        std::process::exit(1);
     }
 
-    println!("Classnames in the TSX file:");
-    for classname in tsx_classnames {
-        println!("{}", classname);
-    }
+    println!("All classnames are defined in the CSS file.");
+    std::process::exit(0);
 }
